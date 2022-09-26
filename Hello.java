3b41585ae097809c37c76b387daf9a1645900e99
김신아 <sinaskim@naver.com>
2022-09-26 16:50:35 +0900

    Main.java 수정 #2, Hello.java 수정 #2

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
-    public void say(){
-        System.out.println("hello");
+    public void say(String msg){
+        System.out.println(msg);
     }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main{
     public static void main(String[] args){
         
         Hello hello = new Hello();
-        hello.say();
+        hello.say("hello world");
     }
 }
