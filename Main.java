2c8207f818455ab39d1369f596ddb2ef349f7e48
김신아 <sinaskim@naver.com>
2022-09-26 16:54:57 +0900

    Main.java 수정 #4

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main{
+
+    static void say(){
+        System.out.println("main");
+    }
     public static void main(String[] args){
-        
+        say();
         Hello hello = new Hello();
         hello.say("hello world");
     }
